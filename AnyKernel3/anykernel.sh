c5732edd32f7c851a5132a208f5b453338e30ac0
vvr3ddy <vvr-rockstar@bmsit.in>
2020-05-05 06:24:43 +0000

    Anykernel : Import freshly
    Signed-off-by: vvr3ddy <vvr-rockstar@bmsit.in>

---
+++ AnyKernel3/anykernel.sh
@@ -0,0 +1,51 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string= Wolf Kernel by vvr-rockstar @xda-developers
+do.devicecheck=1
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=ASUS_X00TD
+device.name2=X00TD
+device.name3=X00T
+device.name4=ASUS_X00T
+device.name5=ASUS_X00TDA
+supported.versions=8.1 - 10.0
+'; } # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+
+
+## AnyKernel install
+dump_boot;
+
+# begin ramdisk changes
+
+# Backup init.rc
+backup_file init.rc;
+
+
+# end ramdisk changes
+
+write_boot;
+## end install
+
